6c7c7d996a41a031eeb559188fd5a8a61350e11f
Miguel <chanampamiguel@gmail.com>
2022-04-26 09:44:20 -0300

    Mostrar tareas pendientes y realizadas

--- tp4_2.c
+++ tp4_2.c
@@ -11,8 +11,11 @@ struct Tarea {
 
 void cargarTareas(Tarea** listadoTareas, int cantidadTareas);
 void cargarTarea(Tarea* tarea, int idTarea);
-void mostrarTareas(Tarea** listadoTareas, int cantidadTareas);
+void mostrarTareas(Tarea** listadoTareas, int cantidadTareas, Tarea** listadoTareasRealizadas);
 void mostrarTarea(Tarea* tarea);
+void mostrarTareasRealizadas(Tarea** listadoTareasRealizadas, int cantidadTareas);
+void mostrarTareasPendientes(Tarea** listadoTareas, int cantidadTareas);
+
 
 int main() {
 
@@ -20,6 +23,7 @@ int main() {
 
     int cantidadTareas;
     Tarea** listadoTareas;
+    Tarea** listadoTareasRealizadas;
 
     printf("Ingrese la cantidad de tareas a realizar: ");
     scanf("%d", &cantidadTareas);
@@ -29,7 +33,11 @@ int main() {
 
     cargarTareas(listadoTareas, cantidadTareas);
 
-    mostrarTareas(listadoTareas, cantidadTareas);
+    mostrarTareas(listadoTareas, cantidadTareas, listadoTareasRealizadas);
+
+    mostrarTareasRealizadas(listadoTareasRealizadas, cantidadTareas);
+    
+    mostrarTareasPendientes(listadoTareas, cantidadTareas);
     
     return 0;
 };
@@ -60,20 +68,53 @@ void cargarTarea(Tarea* tarea, int idTarea) {
     free(Buff);
 };
 
-void mostrarTareas(Tarea** listadoTareas, int cantidadTareas) {
+void mostrarTareas(Tarea** listadoTareas, int cantidadTareas, Tarea** listadoTareasRealizadas) {
 
     for (int i = 0; i < cantidadTareas; i++) {
+        
+        int realizada = 0;
+
         mostrarTarea(listadoTareas[i]);
+
+        printf("La tarea fue realizada? (1: SI 0: NO): ");
+        scanf("%d", &realizada);
+
+        if ( realizada == 1 ) {
+            listadoTareasRealizadas[i] = (Tarea*) malloc( sizeof(Tarea) );
+            listadoTareasRealizadas[i] = listadoTareas[i];
+            listadoTareas[i] = NULL;
+        } else {
+            listadoTareasRealizadas[i] = NULL;
+        };
     };
 };
 
 void mostrarTarea(Tarea* tarea) {
 
-    printf("------------------------------\n");
+    printf("\n------------------------------\n");
 
     printf("ID Tarea: %d\n", tarea->tareaID);
     printf("Descripcion: %s\n", tarea->descripcion);
     printf("Duracion: %d\n", tarea->duracion);
+};
 
-    printf("------------------------------\n\n");
+void mostrarTareasRealizadas(Tarea** listadoTareasRealizadas, int cantidadTareas) {
+
+    printf("\n\n========== Listado de Tareas Realizadas ==========");
+
+    for (int i = 0; i < cantidadTareas; i++) {
+        if (listadoTareasRealizadas[i] != NULL) {
+            mostrarTarea(listadoTareasRealizadas[i]);
+        };
+    };
+};
+
+void mostrarTareasPendientes(Tarea** listadoTareas, int cantidadTareas) {
+    printf("\n\n========== Listado de Tareas Pendientes ==========\n");
+
+    for (int i = 0; i < cantidadTareas; i++) {
+        if (listadoTareas[i] != NULL) {
+            mostrarTarea(listadoTareas[i]);
+        };
+    };
 };
