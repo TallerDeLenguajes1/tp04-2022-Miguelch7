21eac2aca037021fd177d0c3f1e6414214e77eaa
Miguel <chanampamiguel@gmail.com>
2022-04-29 23:50:11 -0300

    Implementación Listas Enlazadas

--- tp4_2.c
+++ tp4_2.c
@@ -9,180 +9,208 @@ struct Tarea {
     int duracion;
 } typedef Tarea;
 
-void cargarTareas(Tarea** listadoTareas, int cantidadTareas);
-void cargarTarea(Tarea* tarea, int idTarea);
-void mostrarTareas(Tarea** listadoTareas, int cantidadTareas, Tarea** listadoTareasRealizadas);
-void mostrarTarea(Tarea* tarea);
-void mostrarTareasRealizadas(Tarea** listadoTareasRealizadas, int cantidadTareas);
-void mostrarTareasPendientes(Tarea** listadoTareas, int cantidadTareas);
-void liberarMemoria(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas);
-Tarea* busquedaPorPalabra(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas, char *palabraClave);
-Tarea* busquedaPorId(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas, int idTarea);
+struct TNodoTarea {
+    Tarea tarea;
+    struct TNodoTarea* siguiente;
+} typedef TNodoTarea;
+
+TNodoTarea* crearListaVacia();
+TNodoTarea* crearTarea(int idTarea);
+void insertarTarea(TNodoTarea** listadoTareas, TNodoTarea* tarea);
+void eliminarTarea(TNodoTarea* listadoTareas, int idTarea);
+void cargarTareas(TNodoTarea** listadoTareas, int cantidadTareas);
+void consultarEstadosTareas(TNodoTarea** listadoTareasPendientes, TNodoTarea** listadoTareasRealizadas);
+void mostrarTarea(Tarea tarea);
+void mostrarTareas(TNodoTarea* listadoTareas);
+TNodoTarea* buscarTareaPorID(TNodoTarea** listadoTareas, int idTarea);
+TNodoTarea* buscarTareaPorPalabra(TNodoTarea** listadoTareas, char* palarbraClave);
 
 int main() {
 
     srand((int) time(NULL));
 
     int cantidadTareas, idTareaABuscar;
-    Tarea** listadoTareas;
-    Tarea** listadoTareasRealizadas;
+    char* buffer;
+    char* palabraClave;
+
+    TNodoTarea* listadoTareasPendientes = crearListaVacia();
+    TNodoTarea* listadoTareasRealizadas = crearListaVacia();    
 
     printf("Ingrese la cantidad de tareas a realizar: ");
     scanf("%d", &cantidadTareas);
     fflush(stdin);
 
-    listadoTareas = (Tarea**) malloc( sizeof(Tarea*) * cantidadTareas );
-    listadoTareasRealizadas = (Tarea**) malloc( sizeof(Tarea*) * cantidadTareas );
+    cargarTareas(&listadoTareasPendientes, cantidadTareas);
 
-    cargarTareas(listadoTareas, cantidadTareas);
+    consultarEstadosTareas(&listadoTareasPendientes, &listadoTareasRealizadas);
 
-    mostrarTareas(listadoTareas, cantidadTareas, listadoTareasRealizadas);
+    printf("\n\n========== Listado de Tareas Realizadas ==========\n");
+    mostrarTareas(listadoTareasRealizadas);
 
-    mostrarTareasRealizadas(listadoTareasRealizadas, cantidadTareas);
-    
-    mostrarTareasPendientes(listadoTareas, cantidadTareas);
+    printf("\n\n========== Listado de Tareas Pendientes ==========\n");    
+    mostrarTareas(listadoTareasPendientes);
 
-    // Búsqueda por id
+    // Búsqueda por ID
     printf("\nIngrese el id de la tarea que desea buscar: ");
     scanf("%d", &idTareaABuscar);
     fflush(stdin);
-    
-    if (busquedaPorId(listadoTareas, listadoTareasRealizadas, cantidadTareas, idTareaABuscar)) {
-        printf("\nLa tarea de id \'%d\' es:", idTareaABuscar);
-        mostrarTarea(busquedaPorId(listadoTareas, listadoTareasRealizadas, cantidadTareas, idTareaABuscar));
+
+    printf("\nEl resultado de la busqueda por ID \'%d\' es:\n", idTareaABuscar);
+
+    // Si el ID no se encuentra entre las tareas pendientes, buscara en las tareas realizadas.
+    if ( buscarTareaPorID(&listadoTareasPendientes, idTareaABuscar) ) {
+        
+        mostrarTarea( buscarTareaPorID(&listadoTareasPendientes, idTareaABuscar)->tarea );
+
+    } else if ( buscarTareaPorID(&listadoTareasRealizadas, idTareaABuscar) ) {
+        
+        mostrarTarea( buscarTareaPorID(&listadoTareasRealizadas, idTareaABuscar)->tarea );
+
     } else {
-        printf("No se ha encontrado una tarea con ID \'%d\'", idTareaABuscar);
+        printf("No se ha encontrado una tarea.\n");
     };
 
     // Búsqueda por palabra clave
-    if (busquedaPorPalabra(listadoTareas, listadoTareasRealizadas, cantidadTareas, "Desarrollar")) {
-        printf("\nLa tarea que contiene la palabra \'Desarrollar\' es:");
-        mostrarTarea(busquedaPorPalabra(listadoTareas, listadoTareasRealizadas, cantidadTareas, "Desarrollar"));
+    buffer = (char*) malloc( sizeof(char) * 50 );
+
+    printf("\nIngrese la palabra clave de la tarea que desea buscar: ");
+    gets(buffer);
+
+    palabraClave = (char*) malloc( sizeof(char) * (strlen(buffer) + 1) );
+    strcpy(palabraClave, buffer);
+    free(buffer);
+    
+    printf("\nEl resultado de la busqueda \'%s\' es:\n", palabraClave);
+    
+    // Si el ID no se encuentra entre las tareas pendientes, buscara en las tareas realizadas.
+    if ( buscarTareaPorPalabra(&listadoTareasPendientes, palabraClave) ) {
+
+        mostrarTarea( buscarTareaPorPalabra(&listadoTareasPendientes, palabraClave)->tarea );
+
+    } else if ( buscarTareaPorPalabra(&listadoTareasRealizadas, palabraClave) ) {
+
+        mostrarTarea( buscarTareaPorPalabra(&listadoTareasRealizadas, palabraClave)->tarea );
+
     } else {
-        printf("No se ha encontrado una tarea con esa palabra");
+        printf("No se ha encontrado una tarea.\n");
     };
 
-    liberarMemoria(listadoTareas, listadoTareasRealizadas, cantidadTareas);
-    
     return 0;
 };
 
-void cargarTareas(Tarea** listadoTareas, int cantidadTareas) {
+TNodoTarea* crearListaVacia() {
+    return NULL;
+};
 
-    for (int i = 0; i < cantidadTareas; i++) {
+TNodoTarea* crearTarea(int idTarea) {
 
-        listadoTareas[i] = (Tarea*) malloc( sizeof(Tarea) );
+    TNodoTarea* nuevaTarea = (TNodoTarea*) malloc( sizeof(TNodoTarea) );
+    char* buff = (char*) malloc( sizeof(char) * 100 );
 
-        cargarTarea(listadoTareas[i], i);
+    nuevaTarea->tarea.tareaID = idTarea;
+
+    printf("Ingrese la descripcion de la tarea: ");
+    gets(buff);
+
+    nuevaTarea->tarea.descripcion = (char*) malloc( sizeof(char) * ( strlen(buff) + 1 ) );
+    strcpy(nuevaTarea->tarea.descripcion, buff);
+    free(buff);
+
+    nuevaTarea->tarea.duracion = 10 + rand() % 91;
+
+    nuevaTarea->siguiente = NULL;
+
+    return nuevaTarea;
+};
+
+void insertarTarea(TNodoTarea** listadoTareas, TNodoTarea* tarea) {
+
+    tarea->siguiente = *listadoTareas;
+
+    *listadoTareas = tarea;
+};
+
+void eliminarTarea(TNodoTarea* listadoTareas, int idTarea) {
+    TNodoTarea* tarea = listadoTareas;
+    TNodoTarea* tareaAnterior = listadoTareas;
+
+    while (tarea && tarea->tarea.tareaID != idTarea) {
+        tareaAnterior = tarea;
+        tarea = tarea->siguiente;
+    };
+
+    if (tarea) {
+        tareaAnterior->siguiente = tarea->siguiente;
     };
 };
 
-void cargarTarea(Tarea* tarea, int idTarea) {
-
-    char* Buff = (char*) malloc( sizeof(char) * 100 );
-
-    tarea->tareaID = idTarea;
-
-    printf("Ingrese la descripcion de la tarea: ");
-    gets(Buff);
-    tarea->descripcion = (char*) malloc( sizeof(char*) * (strlen(Buff) + 1) );
-    strcpy(tarea->descripcion, Buff);
-
-    tarea->duracion = 10 + rand()%91;
-
-    free(Buff);
+void cargarTareas(TNodoTarea** listadoTareas, int cantidadTareas) {
+    for (int i = 0; i < cantidadTareas; i++) {
+        insertarTarea(listadoTareas, crearTarea(i));
+    };
 };
 
-void mostrarTareas(Tarea** listadoTareas, int cantidadTareas, Tarea** listadoTareasRealizadas) {
+void consultarEstadosTareas(TNodoTarea** listadoTareasPendientes, TNodoTarea** listadoTareasRealizadas) {
 
-    for (int i = 0; i < cantidadTareas; i++) {
+    TNodoTarea* tareaAux = *listadoTareasPendientes;
+
+    while (tareaAux) {
         
         int realizada = 0;
 
-        mostrarTarea(listadoTareas[i]);
+        mostrarTarea(tareaAux->tarea);
 
         printf("La tarea fue realizada? (1: SI 0: NO): ");
         scanf("%d", &realizada);
 
         if ( realizada == 1 ) {
-            listadoTareasRealizadas[i] = listadoTareas[i];
-            listadoTareas[i] = NULL;
+            TNodoTarea* tareaRealizadaAux = tareaAux;
+            tareaAux = tareaAux->siguiente;
+            eliminarTarea(*listadoTareasPendientes, tareaRealizadaAux->tarea.tareaID);
+            insertarTarea(listadoTareasRealizadas, tareaRealizadaAux);
         } else {
-            listadoTareasRealizadas[i] = NULL;
+            tareaAux = tareaAux->siguiente;
         };
     };
 };
 
-void mostrarTarea(Tarea* tarea) {
-
+void mostrarTarea(Tarea tarea) {
     printf("\n------------------------------\n");
 
-    printf("ID Tarea: %d\n", tarea->tareaID);
-    printf("Descripcion: %s\n", tarea->descripcion);
-    printf("Duracion: %d\n", tarea->duracion);
+    printf("ID Tarea: %d\n", tarea.tareaID);
+    printf("Descripcion: %s\n", tarea.descripcion);
+    printf("Duracion: %d\n", tarea.duracion);
 };
 
-void mostrarTareasRealizadas(Tarea** listadoTareasRealizadas, int cantidadTareas) {
+void mostrarTareas(TNodoTarea* listadoTareas) {
 
-    printf("\n\n========== Listado de Tareas Realizadas ==========");
+    TNodoTarea* tareaAux = listadoTareas;
 
-    for (int i = 0; i < cantidadTareas; i++) {
-        if (listadoTareasRealizadas[i] != NULL) {
-            mostrarTarea(listadoTareasRealizadas[i]);
-        };
+    while (tareaAux) {
+        mostrarTarea(tareaAux->tarea);
+
+        tareaAux = tareaAux->siguiente;
     };
 };
 
-void mostrarTareasPendientes(Tarea** listadoTareas, int cantidadTareas) {
-    printf("\n\n========== Listado de Tareas Pendientes ==========\n");
-
-    for (int i = 0; i < cantidadTareas; i++) {
-        if (listadoTareas[i] != NULL) {
-            mostrarTarea(listadoTareas[i]);
-        };
-    };
-};
-
-void liberarMemoria(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas) {
+TNodoTarea* buscarTareaPorID(TNodoTarea** listadoTareas, int idTarea) {
     
-    for (int i = 0; i < cantidadTareas; i++) {
+    TNodoTarea* tareaAux = *listadoTareas;
 
-        if (listadoTareas[i] != NULL) {
-            free(listadoTareas[i]->descripcion);
-        };
-
-        if (listadoTareasRealizadas[i] != NULL) {
-            free(listadoTareasRealizadas[i]->descripcion);
-        };
-        
-        free(listadoTareas[i]);
-        free(listadoTareasRealizadas[i]);
+    while (tareaAux && tareaAux->tarea.tareaID != idTarea) {
+        tareaAux = tareaAux->siguiente;
     };
 
-    free(listadoTareas);
-    free(listadoTareasRealizadas);
+    return tareaAux;
 };
 
-Tarea* busquedaPorPalabra(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas, char *palabraClave) {
-    for (int i = 0; i < cantidadTareas; i++) {
-        if (listadoTareas[i] != NULL && strstr(listadoTareas[i]->descripcion, palabraClave) != NULL) {
-            return listadoTareas[i];
-        } else if (listadoTareasRealizadas[i] != NULL && strstr(listadoTareasRealizadas[i]->descripcion, palabraClave) != NULL) {
-            return listadoTareasRealizadas[i];
-        };
+TNodoTarea* buscarTareaPorPalabra(TNodoTarea** listadoTareas, char* palarbraClave) {
+    
+    TNodoTarea* tareaAux = *listadoTareas;
+
+    while (tareaAux && !strstr(tareaAux->tarea.descripcion, palarbraClave)) {
+        tareaAux = tareaAux->siguiente;
     };
 
-    return NULL;
-};
-
-Tarea* busquedaPorId(Tarea** listadoTareas, Tarea** listadoTareasRealizadas, int cantidadTareas, int idTarea) {
-    for (int i = 0; i < cantidadTareas; i++) {
-        if (listadoTareas[i] != NULL && listadoTareas[i]->tareaID == idTarea) {
-            return listadoTareas[i];
-        } else if (listadoTareasRealizadas[i] != NULL && listadoTareasRealizadas[i]->tareaID == idTarea) {
-            return listadoTareasRealizadas[i];
-        };
-    };
-
-    return NULL;
+    return tareaAux;
 };
